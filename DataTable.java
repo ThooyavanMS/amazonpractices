17318715f62c332493576879f48c2d6eb97604c6
ThooyavanMS <thooyavan081@gmail.com>
2023-07-15 15:31:26 +0530

    new code

---
+++ DataTable.java
@@ -0,0 +1,16 @@
+package data.table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataTable {
+public static void main(String[] args) {
+	List<String>emp=new ArrayList<String>();
+	emp.add("R");
+	emp.add("r@");
+	emp.add("java");
+	
+	String st = emp.get(2);
+	System.out.println(st);
+}
+}
